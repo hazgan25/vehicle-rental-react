71be2933080eabc45f8d70a3976ce83d71abb9c0
Hazvi Nurafgan <hazvinurafgan@gmail.com>
2022-04-13 02:57:55 +0700

    del craco

--- craco.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        {
-            // plugin: require('craco-plugin-scoped-css'),
-            plugin: require('craco-plugin-scoped-scss'),
-        },
-    ],
-}
